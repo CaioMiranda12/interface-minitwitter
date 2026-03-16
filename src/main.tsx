9df328fa3f6dbe6df91b3ab29c1edd35a3c886b9
CaioMiranda12 <caioclm04@gmail.com>
2026-03-16 18:08:42 -0300

    feat: config queryclient provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RegisterPage } from './pages/RegisterPage.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RegisterPage />
+    <QueryClientProvider client={queryClient}>
+      <RegisterPage />
+    </QueryClientProvider>
   </StrictMode>,
 )

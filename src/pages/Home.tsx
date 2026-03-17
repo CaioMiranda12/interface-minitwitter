79def86b8e1a17b0fa2539263108c0272e795d0e
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 18:43:41 -0300

    feat: start create post layouts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "@/features/auth/hooks/useAuth";
 import { useLogout } from "@/features/auth/hooks/useLogout";
-import { CiSearch } from "react-icons/ci";
+import { CiHeart, CiSearch } from "react-icons/ci";
+import { HiOutlinePhoto } from "react-icons/hi2";
 import { RiLogoutBoxLine } from "react-icons/ri";
 import { useNavigate } from "react-router";
 
@@ -11,7 +12,7 @@ export function Home() {
 
   return (
     <div>
-      <header className="bg-[#FAFAFA] h-[65px] flex items-center justify-between px-10">
+      <header className="bg-[#FAFAFA] h-[65px] flex items-center justify-between px-10 border-b border-[#E2E8F0]">
         <h1 className="text-primary text-lg font-bold">Mini Twitter</h1>
 
         <div className="flex items-center gap-2 bg-white border border-[#E2E8F0] rounded-lg px-4 py-2 w-[478px]">
@@ -44,7 +45,7 @@ export function Home() {
           </nav>
         ) : (
           <button
-            onClick={logout}
+            onClick={() => logout()}
             className="bg-primary p-2.5 rounded-full
         shadow-[0px_4px_6px_-4px_rgba(13,147,242,0.2),0px_10px_15px_-3px_rgba(13,147,242,0.2)]
         hover:opacity-80 transition-opacity duration-300 active:opacity-60
@@ -53,6 +54,60 @@ export function Home() {
           </button>
         )}
       </header>
+
+      <main className="bg-[#FAFAFA] min-h-[calc(100vh-65px)] flex justify-center">
+        <div className="mt-9 w-[640px] bg-blue-200">
+          <div className="bg-white shadow-md border border-[#E2E8F0] rounded-lg p-4">
+            <textarea placeholder="O que está acontecendo?"
+              className="w-full resize-none outline-none h-[72px]"
+            />
+
+            <div className="flex items-center justify-between border-t border-[#E2E8F0] pt-4">
+              <button>
+                <HiOutlinePhoto size={32} className="text-primary" />
+              </button>
+
+              <button
+                className="bg-primary text-white font-bold text-base rounded-full py-2 px-4
+            shadow-[0px_4px_6px_-4px_rgba(13,147,242,0.2),0px_10px_15px_-3px_rgba(13,147,242,0.2)]
+            hover:opacity-80 transition-opacity duration-300 active:opacity-60
+            "
+              >
+                Postar
+              </button>
+            </div>
+          </div>
+
+          <div className="mt-8 flex flex-col gap-8">
+            <div className="bg-white shadow-md border border-[#E2E8F0] rounded-lg p-4 flex flex-col gap-3">
+              <header className="flex items-center gap-1.5">
+                <h3 className="text-[#314158] font-bold text-base">Lucas Costa</h3>
+                <span className="text-twitterGray font-normal text-sm">@lucascosta</span>
+                <span className="text-twitterGray font-normal text-sm">.</span>
+                <span className="text-twitterGray font-normal text-sm">15/02/2026</span>
+              </header>
+
+              <main>
+                <p className="mb-1 text-[#314158] font-bold text-lg">Iniciando um novo processo seletivo!</p>
+                <p className="text-[#314158] font-normal text-base">
+                  Really excited to share what we've been working on. The team has put in
+                  countless hours to make this seamless. Check out the screenshot below!
+                  #product #launch
+                </p>
+
+                <div className="w-full h-[200px] bg-gray-700 my-3 rounded-lg"></div>
+
+                <button>
+                  <CiHeart size={24} color="red" />
+                </button>
+              </main>
+            </div>
+          </div>
+
+
+
+        </div>
+      </main>
     </div>
   )
 }

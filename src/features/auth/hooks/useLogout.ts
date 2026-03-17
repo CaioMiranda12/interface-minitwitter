a8b33dc1915103b062b45c6b82bdecde974753dd
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 18:43:24 -0300

    fix: update useLogout

--- src/features/auth/hooks/useLogout.ts
+++ src/features/auth/hooks/useLogout.ts
@@ -1,12 +1,21 @@
+import { useMutation } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
+import { logout } from '@/features/auth/services/authService'
 
 export const useLogout = () => {
   const navigate = useNavigate()
 
-  const logout = () => {
-    localStorage.removeItem('token')
-    navigate('/')
-  }
+  const { mutate } = useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      localStorage.removeItem('token')
+      navigate('/')
+    },
+    onError: () => {
+      localStorage.removeItem('token')
+      navigate('/')
+    }
+  })
 
-  return { logout }
+  return { logout: mutate }
 }

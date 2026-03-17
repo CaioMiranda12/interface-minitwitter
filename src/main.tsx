ccef9a2f635d8e89f8de27133f2c3f123f0fa02d
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 16:10:44 -0300

    feat: add react toastify

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AppRoutes from './routes/index.tsx'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 
 const queryClient = new QueryClient()
 
@@ -10,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppRoutes />
+      <ToastContainer pauseOnHover={false} theme="colored" autoClose={1500} />
     </QueryClientProvider>
   </StrictMode>,
 )

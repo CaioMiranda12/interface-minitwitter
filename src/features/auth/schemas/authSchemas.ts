21ad6523e08c0673f730d2ab8c34e9adc8648e56
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 16:17:45 -0300

    feat: passoword request min 4 length

--- src/features/auth/schemas/authSchemas.ts
+++ src/features/auth/schemas/authSchemas.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const registerSchema = z.object({
   name: z.string().min(2, 'Nome deve ter pelo menos 2 caracteres'),
   email: z.string().email('E-mail inválido'),
-  password: z.string().min(6, 'Senha deve ter pelo menos 6 caracteres'),
+  password: z.string().min(4, 'Senha deve ter pelo menos 4 caracteres'),
 })
 
 export type RegisterFormData = z.infer<typeof registerSchema>

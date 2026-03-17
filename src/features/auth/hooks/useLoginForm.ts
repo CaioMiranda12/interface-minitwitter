b66d7530cb7571a1751b95968cefac2892e41053
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 16:49:09 -0300

    feat: create useLoginForm

---
+++ src/features/auth/hooks/useLoginForm.ts
@@ -0,0 +1,24 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { LoginSchema, type LoginFormData } from '@/features/auth/schemas/authSchemas'
+import { useLogin } from './useLogin'
+
+export const useLoginForm = () => {
+  const { mutate, isPending, isError, error } = useLogin()
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    }
+  })
+
+  const onSubmit = handleSubmit((data) => mutate(data))
+
+  return { onSubmit, errors, isPending, isError, error, control }
+}

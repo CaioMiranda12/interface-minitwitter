c837e69182389b95bbe27b6fa5d8c528304cbc8a
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 13:03:22 -0300

    feat: add app routes on main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RegisterPage } from './pages/RegisterPage.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import AppRoutes from './routes/index.tsx'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RegisterPage />
+      <AppRoutes />
     </QueryClientProvider>
   </StrictMode>,
 )

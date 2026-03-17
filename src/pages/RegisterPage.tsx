57e689deb85c7e114b8a56297d865a1b5fa1cf51
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 16:49:34 -0300

    feat: update style on register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -4,17 +4,17 @@ export const RegisterPage = () => {
 
   return (
     <div className="flex justify-center items-center h-[100vh] bg-[#FAFAFA] p-16 sm:p-0">
-      <div>
+      <div className="w-full max-w-[480px]">
         <header>
           <h1 className="text-primary font-bold text-4xl text-center">Mini Twitter</h1>
         </header>
 
         <nav className="mt-[56px] flex">
-          <a href="/login" className="text-primary font-bold text-base flex-grow text-center border-b-2 border-b-primary">
+          <a href="/login" className="text-primary font-bold text-base flex-1 text-center border-b-[3px] border-b-primary">
             Login
           </a>
 
-          <a href="/register" className="text-twitterGray font-bold text-base flex-grow text-center border-b-2 border-b-twitterGray">
+          <a href="/register" className="text-twitterGray font-bold text-base flex-1 text-center border-b border-b-twitterGray">
             Cadastrar
           </a>
         </nav>

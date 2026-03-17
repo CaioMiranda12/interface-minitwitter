a5f81d15828dacb332528b17807aa929502e95dd
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 16:11:04 -0300

    feat: add toast when user register and redirect to /login

--- src/features/auth/hooks/useRegister.ts
+++ src/features/auth/hooks/useRegister.ts
@@ -2,15 +2,21 @@ import { useMutation } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
 import { register } from '@/features/auth/services/authService'
 import type { RegisterFormData } from '@/features/auth/schemas/authSchemas'
+import { toast } from 'react-toastify'
 
 export const useRegister = () => {
   const navigate = useNavigate()
 
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: (data: RegisterFormData) => register(data),
-    onSuccess: (response) => {
-      localStorage.setItem('token', response.token)
-      navigate('/feed')
+    onSuccess: () => {
+      console.log("toast disparado")
+      toast.success('Conta criada com sucesso!')
+
+      setTimeout(() => {
+        navigate('/login')
+      }, 1500)
+
     },
   })
 

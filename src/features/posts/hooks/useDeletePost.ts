5e956dfbd744db5e2ecb606b118cbf7967c5705a
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 19:42:46 -0300

    feat: create useDelete post

---
+++ src/features/posts/hooks/useDeletePost.ts
@@ -0,0 +1,13 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { deletePost } from '@/features/posts/services/postsService'
+
+export const useDeletePost = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (id: string) => deletePost(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] })
+    },
+  })
+}

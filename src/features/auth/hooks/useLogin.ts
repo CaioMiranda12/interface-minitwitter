7ea82d535a29fff55fbdcf8060fb96eba2072fa3
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 16:49:01 -0300

    feat: create useLogin

---
+++ src/features/auth/hooks/useLogin.ts
@@ -0,0 +1,31 @@
+import { useMutation } from '@tanstack/react-query'
+import { useNavigate } from 'react-router-dom'
+import { login } from '@/features/auth/services/authService'
+import type { LoginFormData } from '@/features/auth/schemas/authSchemas'
+import { toast } from 'react-toastify'
+
+export const useLogin = () => {
+  const navigate = useNavigate()
+
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: (data: LoginFormData) => login(data),
+    onSuccess: (response) => {
+      if (!response.token) {
+        toast.error('Erro ao autenticar')
+        return
+      }
+
+      localStorage.setItem('token', response.token)
+
+      toast.success('Seja bem-vindo ao Mini-Twitter!')
+      navigate('/feed')
+    },
+
+    onError: (error: any) => {
+      const message = error.response?.data?.message || 'Erro ao fazer login.'
+      toast.error(message)
+    }
+  })
+
+  return { mutate, isPending, isError, error }
+}

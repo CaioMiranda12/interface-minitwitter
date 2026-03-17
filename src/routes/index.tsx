5835316bc8b252e2715f27180275d64252834e19
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 16:49:44 -0300

    feat: add component on login route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { RegisterPage } from '@/pages/RegisterPage'
+import { LoginPage } from '@/pages/LoginPage'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<div>Login (em breve)</div>} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/feed" element={<div>Feed (em breve)</div>} />
       </Routes>
     </BrowserRouter>

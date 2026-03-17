a546874ecaa033870a41278a3a8337a1e0d8a1ec
CaioMiranda12 <caioclm04@gmail.com>
2026-03-17 16:19:35 -0300

    feat: create login schema

--- src/features/auth/schemas/authSchemas.ts
+++ src/features/auth/schemas/authSchemas.ts
@@ -6,4 +6,10 @@ export const registerSchema = z.object({
   password: z.string().min(4, 'Senha deve ter pelo menos 4 caracteres'),
 })
 
+export const LoginSchema = z.object({
+  email: z.string().email('E-mail inválido'),
+  password: z.string().min(4, 'Senha deve ter pelo menos 4 caracteres'),
+})
+
 export type RegisterFormData = z.infer<typeof registerSchema>
+export type LoginFormData = z.infer<typeof LoginSchema>

f39e317cc0124c5b3927717a9f95af7150808e65
CaioMiranda12 <caioclm04@gmail.com>
2026-03-18 14:45:16 -0300

    feat: return usr on useAuth

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -1,5 +1,9 @@
 export const useAuth = () => {
   const token = localStorage.getItem('token')
+  const user = localStorage.getItem('user')
 
-  return { isAuthenticated: !!token }
+  return {
+    isAuthenticated: !!token,
+    user: user ? JSON.parse(user) : null,
+  }
 }

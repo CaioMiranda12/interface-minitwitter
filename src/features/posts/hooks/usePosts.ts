d6f4531f454d0c4b17c7310eeb97810c06b34719
CaioMiranda12 <caioclm04@gmail.com>
2026-03-18 12:30:25 -0300

    feat: add pagination calc on usePosts

--- src/features/posts/hooks/usePosts.ts
+++ src/features/posts/hooks/usePosts.ts
@@ -2,8 +2,14 @@ import { useQuery } from '@tanstack/react-query'
 import { getPosts, type GetPostsParams } from '@/features/posts/services/postsService'
 
 export const usePosts = (params?: GetPostsParams) => {
-  return useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ['posts', params],
-    queryFn: async () => await getPosts(params)
+    queryFn: () => getPosts(params),
   })
+
+  const totalPages = data ? Math.ceil(data.total / data.limit) : 0
+  const hasNextPage = (params?.page ?? 1) < totalPages
+  const hasPrevPage = (params?.page ?? 1) > 1
+
+  return { data, isLoading, isError, totalPages, hasNextPage, hasPrevPage }
 }

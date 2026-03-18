f0e5e93bde121aed0eba8f4fca0773c2c84d100c
CaioMiranda12 <caioclm04@gmail.com>
2026-03-18 12:31:03 -0300

    feat: create uploadImage lib with imgbb

---
+++ src/lib/uploadImage.ts
@@ -0,0 +1,12 @@
+export const uploadImage = async (file: File): Promise<string> => {
+  const formData = new FormData()
+  formData.append('image', file)
+
+  const response = await fetch(
+    `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,
+    { method: 'POST', body: formData }
+  )
+
+  const data = await response.json()
+  return data.data.url
+}

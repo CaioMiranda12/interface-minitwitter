1345bd32b9de5bbbbf3691e23d533d7d51c0fceb
CaioMiranda12 <caioclm04@gmail.com>
2026-03-18 12:30:34 -0300

    feat: add createPostSchema

---
+++ src/features/posts/schemas/postSchemas.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+
+export const createPostSchema = z.object({
+  title: z.string().min(3, 'O título deve ter no minimo 3 caracteres'),
+  content: z.string().min(1, 'Conteúdo é obrigatório'),
+  image: z
+    .instanceof(FileList)
+    .optional()
+    .refine(
+      (files) => !files || files.length === 0 || files[0].size <= 5 * 1024 * 1024,
+      'A imagem deve ter no máximo 5MB'
+    ),
+})
+
+export type CreatePostFormData = z.infer<typeof createPostSchema>

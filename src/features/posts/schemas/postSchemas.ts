3358018705afb666810acc25e6cb329113766962
CaioMiranda12 <caioclm04@gmail.com>
2026-03-18 15:38:11 -0300

    feat: add edit post schena

--- src/features/posts/schemas/postSchemas.ts
+++ src/features/posts/schemas/postSchemas.ts
@@ -12,4 +12,18 @@ export const createPostSchema = z.object({
     ),
 })
 
+export const editPostSchema = z.object({
+  title: z.string().min(3, 'O título deve ter no minimo 3 caracteres'),
+  content: z.string().min(1, 'Conteúdo é obrigatório'),
+  image: z
+    .instanceof(FileList)
+    .optional()
+    .refine(
+      (files) => !files || files.length === 0 || files[0].size <= 5 * 1024 * 1024,
+      'A imagem deve ter no máximo 5MB'
+    ),
+})
+
 export type CreatePostFormData = z.infer<typeof createPostSchema>
+
+export type EditPostFormData = z.infer<typeof editPostSchema>

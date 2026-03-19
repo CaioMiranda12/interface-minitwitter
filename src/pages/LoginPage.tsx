4f1166735c47fde13b84a6cb5ed39750e9e57fa9
CaioMiranda12 <caioclm04@gmail.com>
2026-03-19 17:40:15 -0300

    feat: add dark theme on login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,22 +4,22 @@ import { NavLink } from "react-router-dom"
 export const LoginPage = () => {
 
   return (
-    <div className="flex justify-center items-center h-[100vh] bg-[#FAFAFA] p-16 sm:p-0">
+    <div className="flex justify-center items-center h-[100vh] bg-[#FAFAFA] p-16 sm:p-0 dark:bg-[#070B14]">
       <div className="w-full max-w-[480px]">
         <header>
-          <h1 className="text-primary font-bold text-4xl text-center">Mini Twitter</h1>
+          <h1 className="text-primary font-bold text-4xl text-center dark:text-white">Mini Twitter</h1>
         </header>
 
         <nav className="mt-[56px] flex">
           <NavLink to="/login" className={({ isActive }) => `font-bold text-base flex-1 text-center pb-1
-          ${isActive ? 'text-primary border-b-primary border-b-[3px]' : 'text-twitterGray border-b-twitterGray border-b'}
+          ${isActive ? 'text-primary border-b-primary border-b-[3px] dark:text-white dark:border-b-white' : 'text-twitterGray border-b-twitterGray border-b dark:text-[#90A1B9]'}
           `
           }>
             Login
           </NavLink>
 
           <NavLink to="/register" className={({ isActive }) => `font-bold text-base flex-1 text-center pb-1
-          ${isActive ? 'text-primary border-b-primary border-b-[3px]' : 'text-twitterGray border-b-twitterGray border-b'}
+          ${isActive ? 'text-primary border-b-primary border-b-[3px] dark:text-white dark:border-b-white' : 'text-twitterGray border-b-twitterGray border-b dark:text-[#90A1B9]'}
           `
           }>
             Cadastrar
@@ -27,8 +27,8 @@ export const LoginPage = () => {
         </nav>
 
         <main className="mt-6">
-          <h2 className="text-primary text-3xl font-semibold">Olá, de novo!</h2>
-          <p className="text-twitterGray text-base font-normal">Por favor, insira os seus dados para fazer login.</p>
+          <h2 className="text-primary text-3xl font-semibold dark:text-white">Olá, de novo!</h2>
+          <p className="text-twitterGray text-base font-normal dark:text-[#90A1B9]">Por favor, insira os seus dados para fazer login.</p>
 
           <div className="mt-6">
             <LoginForm />

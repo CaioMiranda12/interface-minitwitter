be1c62a43307e50bded84e9aea09ed9e40133cfe
CaioMiranda12 <caioclm04@gmail.com>
2026-03-19 13:31:12 -0300

    feat: use signOut on useLogout

--- src/features/auth/hooks/useLogout.ts
+++ src/features/auth/hooks/useLogout.ts
@@ -1,14 +1,16 @@
 import { useMutation } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
 import { logout } from '@/features/auth/services/authService'
+import { useAuthContext } from '@/context/AuthContext'
 
 export const useLogout = () => {
   const navigate = useNavigate()
+  const { signOut } = useAuthContext()
 
   const { mutate } = useMutation({
     mutationFn: logout,
     onSuccess: () => {
-      localStorage.removeItem('token')
+      signOut()
       navigate('/')
     },
     onError: () => {

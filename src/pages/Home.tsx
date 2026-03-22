c7b6ef1aa116430f46d1a49fa7010dcfd7fb88bd
CaioMiranda12 <caioclm04@gmail.com>
2026-03-22 12:53:20 -0300

    feat: use search input on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/Header";
+import { SearchInput } from "@/components/SearchInput";
 import { PostForm } from "@/features/posts/components/PostForm";
 import { Timeline } from "@/features/posts/components/Timeline";
 
@@ -8,7 +9,11 @@ export function Home() {
     <>
       <Header />
 
-      <main className="bg-[#FAFAFA] min-h-[calc(100vh-65px)] flex justify-center dark:bg-[#070B14]">
+      <div className="2xl:hidden flex justify-center px-3 py-2 border-b border-[#E2E8F0] dark:border-[#62748E] bg-[#FAFAFA] dark:bg-[#070B14]">
+        <SearchInput className="w-[640px]" />
+      </div>
+
+      <main className="bg-[#FAFAFA] min-h-[calc(100vh-65px)] p-3 md:p-0 flex justify-center dark:bg-[#070B14]">
         <div className="mt-9 w-[640px]">
           <PostForm />
 

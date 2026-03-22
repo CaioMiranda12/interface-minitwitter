309d1e07f650db7e362af7f781e3bbb33c9a0263
CaioMiranda12 <caioclm04@gmail.com>
2026-03-22 20:12:38 -0300

    feat: add onError on useEditPost and useDeletePost

--- src/features/posts/hooks/useDeletePost.ts
+++ src/features/posts/hooks/useDeletePost.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { deletePost } from '@/features/posts/services/postsService'
+import { toast } from 'react-toastify'
 
 export const useDeletePost = () => {
   const queryClient = useQueryClient()
@@ -9,5 +10,10 @@ export const useDeletePost = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['posts'] })
     },
+    onError: (error: any) => {
+      if (error?.response?.status === 403) {
+        toast.error('Você não tem permissão para realizar essa ação.')
+      }
+    }
   })
 }
--- src/features/posts/hooks/useEditPost.ts
+++ src/features/posts/hooks/useEditPost.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { editPost, type CreatePostData } from '@/features/posts/services/postsService'
+import { toast } from 'react-toastify'
 
 export const useEditPost = () => {
   const queryClient = useQueryClient()
@@ -10,5 +11,10 @@ export const useEditPost = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['posts'] })
     },
+    onError: (error: any) => {
+      if (error?.response?.status === 403) {
+        toast.error('Você não tem permissão para realizar essa ação.')
+      }
+    }
   })
 }

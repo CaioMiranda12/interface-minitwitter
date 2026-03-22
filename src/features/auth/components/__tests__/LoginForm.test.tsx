2926b1e34c36f0bb6bff9c97a2b24b8a550416fd
CaioMiranda12 <caioclm04@gmail.com>
2026-03-22 20:00:18 -0300

    feat: add test on login form: alterna visibilidade da senha ao clicar no ícone do olho

--- src/features/auth/components/__tests__/LoginForm.test.tsx
+++ src/features/auth/components/__tests__/LoginForm.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { LoginForm } from '../LoginForm'
 import { describe, it, expect, vi } from 'vitest'
 import { BrowserRouter } from 'react-router-dom'
@@ -36,5 +36,21 @@ describe('LoginForm', () => {
   it('botão não está desabilitado por padrão', () => {
     render(<LoginForm />, { wrapper })
     expect(screen.getByRole('button', { name: /continuar/i })).not.toBeDisabled()
+  })
+
+  it('alterna visibilidade da senha ao clicar no ícone do olho', async () => {
+    render(<LoginForm />, { wrapper })
+
+    const passwordInput = screen.getByPlaceholderText('Insira a sua senha')
+
+    expect(passwordInput).toHaveAttribute('type', 'password')
+
+    const toggleButton = screen.getByTestId('toggle-password')
+    fireEvent.click(toggleButton)
+
+    expect(passwordInput).toHaveAttribute('type', 'text')
+
+    fireEvent.click(toggleButton)
+    expect(passwordInput).toHaveAttribute('type', 'password')
   })
 })

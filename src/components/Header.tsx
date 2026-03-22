2a7dc506b98e937e6c7c6569b58ca293ad91f6da
CaioMiranda12 <caioclm04@gmail.com>
2026-03-22 12:40:21 -0300

    style: fix header repositivty

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import { CiSearch } from "react-icons/ci";
 import { RiLogoutBoxLine } from "react-icons/ri";
 import { useNavigate, useSearchParams } from "react-router";
 import { ThemeButton } from "./ThemeButton";
+import { Menu } from "./Menu";
 
 export function Header() {
   const navigate = useNavigate();
@@ -28,10 +29,12 @@ export function Header() {
   }
 
   return (
-    <header className="bg-[#FAFAFA] h-[65px] flex items-center justify-between px-10 border-b border-[#E2E8F0] dark:bg-[#070B14] dark:border-[#62748E]">
-      <h1 className="text-primary text-lg font-bold dark:text-white">Mini Twitter</h1>
+    <header className="bg-[#FAFAFA] h-[65px] flex items-center justify-between px-3 md:px-10 border-b border-[#E2E8F0]  dark:bg-[#070B14] dark:border-[#62748E]">
+      <div className="flex-1">
+        <h1 className="text-primary text-lg font-bold dark:text-white">Mini Twitter</h1>
+      </div>
 
-      <div className="flex items-center gap-2 bg-white border border-[#E2E8F0] rounded-lg px-4 py-2 w-[478px] dark:bg-[#1D293D] dark:border-none dark:text-white">
+      <div className="hidden 2xl:flex w-[640px] items-center gap-2 bg-white border border-[#E2E8F0] rounded-lg px-4 py-2 dark:bg-[#1D293D] dark:border-none dark:text-white">
         <button onClick={handleSearch}>
           <CiSearch size={18} color="#62748E" />
         </button>
@@ -46,38 +49,44 @@ export function Header() {
         />
       </div>
 
-      <div className="flex gap-2">
-        {!isAuthenticated ? (
-          <nav>
-            <button onClick={() => navigate('/register')}
-              className="text-twitterGray font-bold text-base border border-[#E2E8F0] rounded-full py-2 w-[156px] dark:text-white dark:border-[#62748E]
+      <div className="flex-1 flex justify-end gap-2">
+        <div className="hidden md:flex gap-2">
+          {!isAuthenticated ? (
+            <nav className="flex gap-2">
+              <button onClick={() => navigate('/register')}
+                className="text-twitterGray font-bold text-base border border-[#E2E8F0] rounded-full py-2 w-[156px] dark:text-white dark:border-[#62748E]
           hover:bg-twitterGray hover:text-white transition-colors duration-300 active:bg-twitterGray/80 active:text-white
           ">
-              Registrar-se
-            </button>
+                Registrar-se
+              </button>
 
-            <button
-              onClick={() => navigate('/login')}
-              className="text-white font-bold text-base bg-primary rounded-full py-2 w-[156px]
+              <button
+                onClick={() => navigate('/login')}
+                className="text-white font-bold text-base bg-primary rounded-full py-2 w-[156px]
           shadow-[0px_4px_6px_-4px_rgba(13,147,242,0.2),0px_10px_15px_-3px_rgba(13,147,242,0.2)]
           hover:opacity-80 transition-opacity duration-300 active:opacity-60
           ">
-              Login
-            </button>
-          </nav>
-        ) : (
-          <button
-            onClick={() => logout()}
-            className="bg-primary p-2.5 rounded-full dark:bg-[#1D293D]
+                Login
+              </button>
+            </nav>
+          ) : (
+            <button
+              onClick={() => logout()}
+              className="bg-primary p-2.5 rounded-full dark:bg-[#1D293D]
         shadow-[0px_4px_6px_-4px_rgba(13,147,242,0.2),0px_10px_15px_-3px_rgba(13,147,242,0.2)]
-        hover:opacity-80 transition-opacity duration-300 active:opacity-60
-        ">
-            <RiLogoutBoxLine size={20} color='white' />
-          </button>
-        )}
+        hover:opacity-80 transition-opacity duration-300 active:opacity-60"
+            >
+              <RiLogoutBoxLine size={20} color='white' />
+            </button>
+          )}
 
-        <ThemeButton />
+          <ThemeButton />
+        </div>
+
+        <Menu />
       </div>
+
+
     </header>
   )
 }
